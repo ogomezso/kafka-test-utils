2c7e4a2a1756c0bcf9c7f2c779a039db1e8977fb
Oscar Gomez <ogomezso0@gmail.com>
2022-04-26 13:54:59 +0200

    kafka test producer list of proper produced records updated

--- src/main/java/es/santander/kafka/test/clients/KafkaTestProducer.java
+++ src/main/java/es/santander/kafka/test/clients/KafkaTestProducer.java
@@ -9,6 +9,7 @@ import es.santander.kafka.test.objects.TestRecord;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 public abstract class KafkaTestProducer<K, V> {
@@ -28,10 +29,10 @@ public abstract class KafkaTestProducer<K, V> {
     public List<ProducerRecord<K, V>> produceMessages(String topicName, List<TestRecord<K, V>> messages) {
         List<ProducerRecord<K, V>> recordsProduced = new ArrayList<>();
         messages.forEach(message -> {
-            ProducerRecord<K, V> msg = Optional.ofNullable(message.getKey())
-                    .map(k -> sendMessage(topicName, message.getKey(), message.getValue()))
-                    .orElseGet(() -> sendMessageWithoutKey(topicName, message.getValue()));
-            recordsProduced.add(msg);
+            Optional<ProducerRecord<K, V>> msg = Optional.ofNullable(message.getKey())
+                    .map(key -> sendMessage(new ProducerRecord<>(topicName, message.getKey(), message.getValue())))
+                    .orElseGet(() -> sendMessage(new ProducerRecord<>(topicName, message.getValue())));
+            msg.ifPresent(recordsProduced::add);
         });
         plainProducer.flush();
         List<ProducerRecord<K, V>> processedRecords = processResult(recordsProduced);
@@ -39,27 +40,14 @@ public abstract class KafkaTestProducer<K, V> {
         return processedRecords;
     }
 
-    private ProducerRecord<K, V> sendMessage(String topicName, K key, V value) {
-        ProducerRecord<K, V> record = new ProducerRecord<>(topicName, key, value);
-        plainProducer.send(record, (recordMetadata, exception) -> {
-            if (exception == null) {
-                log.debug("Record written {}", record);
-            } else {
-                handleError(exception, record);
-            }
-        });
-        return record;
+    private Optional<ProducerRecord<K, V>> sendMessage(ProducerRecord<K,V> record) {
+        try {
+            plainProducer.send(record).get();
+            return Optional.of(record);
+        } catch (InterruptedException | ExecutionException e) {
+            handleError(e, record);
+        }
+        return Optional.empty();
     }
 
-    private ProducerRecord<K, V> sendMessageWithoutKey(String topicName, V value) {
-        ProducerRecord<K, V> record = new ProducerRecord<>(topicName, value);
-        plainProducer.send(record, (recordMetadata, exception) -> {
-            if (exception == null) {
-                log.debug("Record written {}", record);
-            } else {
-                handleError(exception, record);
-            }
-        });
-        return record;
-    }
 }
